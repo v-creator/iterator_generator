188bd4627095d2e92fa5e990dc67b69c738e5973
17845001 <Kolesnikov.V.Aleks@sberbank.ru>
2022-12-12 17:19:16 +0300

    update task_1

--- task1_.py
+++ task1_.py
@@ -3,7 +3,7 @@ from functools import reduce
 class FlatIterator:
 
     def __init__(self, list_of_list):
-        self.list_of_list = reduce(lambda x,y: x + y, list_of_list)
+        self.list_of_list = list_of_list
 
     def __iter__(self):
         self.count = -1
@@ -11,9 +11,9 @@ class FlatIterator:
 
     def __next__(self):
         self.count += 1
-        if self.count == len(self.list_of_list):
+        if self.count == len([self.list_of_list[0] + self.list_of_list[1] + self.list_of_list[2]][0]):
             raise StopIteration
-        return self.list_of_list[self.count]
+        return [self.list_of_list[0] + self.list_of_list[1] + self.list_of_list[2]][0][self.count]
 
 
 def test_1():
@@ -22,12 +22,13 @@ def test_1():
         ['d', 'e', 'f', 'h', False],
         [1, 2, None]
     ]
+
     for flat_iterator_item, check_item in zip(
             FlatIterator(list_of_lists_1),
             ['a', 'b', 'c', 'd', 'e', 'f', 'h', False, 1, 2, None]
     ):  
         assert flat_iterator_item == check_item
-
+    
     assert list(FlatIterator(list_of_lists_1)) == ['a', 'b', 'c', 'd', 'e', 'f', 'h', False, 1, 2, None]
 
 
